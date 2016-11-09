f99099479d2b657a3a2a73f74bb00de5b79684eb
nafeng <765628993@qq.com>
2016-11-09 21:55:19 +0800

    贪吃蛇争霸赛

---
+++ js/core.js
@@ -0,0 +1,258 @@
+//------------------------------------------------------------------------------------------------------------------------------游戏核心脚本
+var s;
+var $=document.querySelector;
+var score;
+var headX = 15,headY = 15,headL = 10;//snakes 单个块体的边长
+var nodirection;
+var direction;
+var t1; //变量写在这里就可以保证 被清除了
+var c = document.getElementById('canvas');
+var head = c.getContext("2d");  //头部
+head.fillstyle = "#000";
+/*function cnvs_getCoordinates(e)
+{
+x=e.clientX;
+y=e.clientY;
+document.getElementById("xycoordinates").innerHTML="Coordinates: (" + x + "," + y + ")";
+}
+ 
+function cnvs_clearCoordinates()
+{
+document.getElementById("xycoordinates").innerHTML="";
+}
+//获取鼠标坐标*/
+var score_num = document.getElementById("score_num");
+var up = document.getElementById('up');
+var right = document.getElementById('right');
+var down = document.getElementById('down');
+var left = document.getElementById('left');
+//获取控制元素
+function draw(){
+	window.setTimeout(function(){
+		window.clearInterval(t1);
+	},50); 
+	s = new Array();//蛇的身体 数组snake
+	var food = {
+		x:0,
+		y:0
+	};
+	var  x,y,l=10;
+	s.push({
+		x:15,
+		y:15
+	});
+	score_num.innerHTML = 0;
+	//游戏初始化
+	function drawFood(){
+		var mrx = Math.random()*25;
+		var mry = Math.random()*20;
+		food = {
+			x:Math.floor(mrx)*l+l/2,
+			y:Math.floor(mry)*l+l/2
+		};
+		var foodResult = true;
+		for (var i = 0; i < s.length ; i++) {
+			if ( s[i].x == food.x && s[i].y == food.y) {
+				foodResult = false;
+				break;
+			}
+		}
+		if (foodResult){
+			head.beginPath();
+			head.strokeStyle = 'rgba(255,0,0,1)';
+			head.arc(food.x,food.y,3,0,Math.PI*2,true);// 画食物
+			head.stroke();
+		}else{
+			drawFood(); //递归
+		}
+	}//画食物
+	drawFood();
+
+	
+	function clearRect(x,y,a){
+	var b=a/2;
+	head.clearRect (x-b, y-b, a, a);
+	}//清除尾部
+
+	function rect(x,y,a){
+	var b=a/2;
+	head.fillRect (x-b, y-b, a, a);
+	}//画一个单个块体
+
+	rect(headX,headY,headL);//画出初始块体
+
+	document.onkeydown = function (event){
+		
+		var e = event || window.event || arguments.callee.caller.arguments[0];
+		if ((e && e.keyCode == nodirection )|| ( e.keyCode != 38 && e.keyCode != 39 && e.keyCode != 40 && e.keyCode != 37)) { return 0 ;}// 如果输入相反方向则直接跳出
+		window.clearInterval(t1);
+		function go()
+		{	
+			if (s.length-5>0) {
+				score = s.length-5
+			} else{
+				score = 0;
+			}
+			if (e && e.keyCode == 38) {
+				s.unshift({
+					x:s[0].x,
+					y:s[0].y - l
+				});	
+				direction = "38";
+				nodirection = "40";				
+			}
+			if (e && e.keyCode == 39) {
+				s.unshift({
+					x:s[0].x + l,
+					y:s[0].y
+				});	
+				direction = "39";
+				nodirection = "37";
+			}
+			if (e && e.keyCode == 40) {
+				s.unshift({
+					x:s[0].x,
+					y:s[0].y + l
+				});	
+				direction = "40";
+				nodirection = "38";
+			}
+			if (e && e.keyCode == 37) {
+				s.unshift({
+					x:s[0].x - l,
+					y:s[0].y
+				});	
+				direction = "37";
+				nodirection = "39";
+			}
+			
+			if (s.length > 5){
+				if (s[0].x == food.x && s[0].y ==food.y){
+					drawFood();
+					score_num.innerHTML = s.length - 5 ;
+				}
+				else{
+					clearRect(s[s.length - 1].x,s[s.length - 1].y,l);
+					s.pop();
+				}
+			}
+			for (var i = 1; i < s.length; i++) {
+				if(s[i].x ==s[0].x && s[i].y == s[0].y){
+					head.clearRect(0,0,250,200);
+					pw("game over! 你的得分是："+score);
+					saveData(username, score);
+					draw();
+					nodirection = null;
+					window.clearInterval(t1);
+				}
+			}
+						
+			if (s[0].x<0 || s[0].x>250 || s[0].y<0 ||s[0].y>200){
+				head.clearRect(0,0,250,200);
+				pw("game over! 你的得分是："+score);
+				saveData(username, score);
+				draw();
+				nodirection = null;
+			}
+			rect(s[0].x,s[0].y,l);
+		}
+		go();
+		t1 = window.setInterval(go,300-s.length);//加速功能
+
+
+	} //键盘事件 上下左右
+
+	//手机端点击触发移动
+	function move(event){
+			var e = event || window.event || arguments.callee.caller.arguments[0];
+			if ((e == nodirection )|| ( e != 38 && e != 39 && e != 40 && e != 37)) { return 0 ;}// 如果输入相反方向则直接跳出
+			window.clearInterval(t1);
+			function go()
+			{	
+				if (s.length-5>0) {
+					score = s.length-5
+				} else{
+					score = 0;
+				}
+				if (e == 38) {
+					s.unshift({
+						x:s[0].x,
+						y:s[0].y - l
+					});	
+					direction = "38";
+					nodirection = "40";			
+				}
+				if (e == 39) {
+					s.unshift({
+						x:s[0].x + l,
+						y:s[0].y
+					});	
+					direction = "39";
+					nodirection = "37";
+				}
+				if (e == 40) {
+					s.unshift({
+						x:s[0].x,
+						y:s[0].y + l
+					});	
+					direction = "40";
+					nodirection = "38";
+				}
+				if (e == 37) {
+					s.unshift({
+						x:s[0].x - l,
+						y:s[0].y
+					});	
+					direction = "37";
+					nodirection = "39";
+				}
+				if (s.length > 5){
+					if (s[0].x == food.x && s[0].y ==food.y){
+						drawFood();
+						score_num.innerHTML = s.length - 5 ;
+					}
+					else{
+						clearRect(s[s.length - 1].x,s[s.length - 1].y,l);
+						s.pop();
+					}
+				}
+				for (var i = 1; i < s.length; i++) {
+					if(s[i].x ==s[0].x && s[i].y == s[0].y){
+						head.clearRect(0,0,250,200);
+						pw("game over! 你的得分是："+score);
+						saveData(username, score);
+						draw();
+						nodirection = null;
+						window.clearInterval(t1);
+					}
+				}					
+				if (s[0].x<0 || s[0].x>250 || s[0].y<0 ||s[0].y>200){
+					head.clearRect(0,0,250,200);
+					pw("game over! 你的得分是："+score);
+					saveData(username, score);
+					draw();
+					nodirection = null;
+				}
+				rect(s[0].x,s[0].y,l);
+			}
+			go();
+			t1 = window.setInterval(go,300-s.length);//加速功能
+
+
+		} //点击事件 上下左右
+
+	up.onclick = function(){
+		move(38);
+	};
+	right.onclick =function(){
+		move(39);
+	};
+	down.onclick = function(){
+		move(40);
+	};
+	left.onclick =function(){
+		move(37);
+	};
+
+}
+draw();//开始游戏
---
+++ js/no_core.js
@@ -0,0 +1,115 @@
+//-------------------------------------------------------------------------------------游戏非核心脚本
+
+
+  var username;
+  var APP_ID = 'e2rLuu1INqllK9LDacETyAtk-gzGzoHsz';
+  var APP_KEY = 'pgIsLV4lEp2JQ8riAJbIFnOm';
+
+  AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+
+  });
+
+  var saveData = function(name, score) {
+    var Range = AV.Object.extend('Range');
+    var range = new Range();
+    range.set('name', String(name));
+    range.set('score', String(score));
+    range.save().then(function(data) {
+      console.log('New object created with objectId: ' + range.id);
+    }, function(error) {
+      console.error('Failed to create new object, with error message: ' + range.message);
+    });
+  }
+
+  var getData = function() {
+    document.getElementById('rank_title').innerHTML = "请稍等......";
+    var query = new AV.Query('Range');
+
+    function compare(property) {
+      return function(a, b) {
+        var value1 = a[property];
+        var value2 = b[property];
+        return value2 - value1;
+      }
+    }
+
+    function nameInArr(name, arr) {
+      for (var j = 0, length2 = arr.length; j < length2; j++) {
+        if (arr[j].name == name) {
+          return true;
+        }
+      }
+    }
+    query.find().then(function(data) {
+      var finalAlluser = [];
+      var allUser = [];
+      var finalStr = "";
+      data.forEach(function(item) {
+        allUser.push({
+          name: item.attributes.name,
+          score: item.attributes.score
+        })
+      });
+      allUser.sort(compare('score'));
+      for (var i = 0, length1 = allUser.length; i < length1; i++) {
+        if (i === 0) {
+          finalAlluser.push(allUser[0]);
+        } else {
+          if (!nameInArr(allUser[i].name, finalAlluser)) {
+            finalAlluser.push(allUser[i]);
+          }
+        }
+      }
+      var k = 0 ;//只取前10名
+      finalAlluser.forEach(function(item) {
+        if (k<10) {
+        	if (k<3) {
+        		finalStr += "<strong><li>" + item.name + ":" + item.score + "</li></strong>";
+        	}
+        	else{finalStr += "<li>" + item.name + ":" + item.score + "</li>";}
+        }
+        k++;
+      })
+      document.getElementById("rank_title").innerHTML = "排行榜";
+      document.querySelector('#rank_list ol').innerHTML = finalStr;
+    })
+  }
+//---------------------------------------------------------------------------以上是排名系统
+var pushword = document.getElementById("pushword");
+var pushword_word = document.getElementById("pushword_word");
+var login = document.getElementById("login");
+window.addEventListener('load', function () {
+	FastClick.attach(document.body);
+}, false);
+document.getElementById("rank_title").onclick = function(){
+	var rc = document.getElementById("rank_content");
+	if (rc.style.display != "none") {
+		rc.style.display = "none";
+	}
+	else{
+		getData();
+		rc.style.display = "block";
+	}
+};
+document.getElementById("go").onclick = function(){
+	var name = document.getElementById("name").value;
+	//var qq = document.getElementById("qq").value;
+	if ( /*qq != "" &&*/ name != "" ){
+		username = name;
+		login.style.opacity = "0";
+		setTimeout("login.style.zIndex = '-1';",500);
+	}
+	else{
+		pw("请完成表单后开始游戏~");
+	}
+}
+document.getElementById("pushword_ok").onclick = function(){
+	pushword.style.display = "none";
+}
+function pw(a){
+	var txt = a;
+	pushword_word.innerHTML = txt;
+	pushword.style.display = "block";
+}
